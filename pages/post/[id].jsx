319b5bbe83101d3af17899ce591cc7fb1560d123
Afx31 <post4wjm@gmail.com>
2022-09-29 18:47:34 +1000

    testing getStaticProps 2

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -62,9 +62,25 @@ const ViewPostPage = (props) => {
   )
 }
 
-export const getStaticProps = async (context) => {
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const res = await await prisma.post.findMany()
+  const posts = await res.json()
+
+  // Get the paths we want to pre-render based on posts
+  const paths = posts.map((post) => ({
+    params: { id: post.id },
+  }))
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
+}
+
+export const getStaticProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
-    where: { id: Number(context.params.id) }
+    where: { id: Number(params.id) }
   })
   
   const recentPostLinks = await prisma.post.findMany({
