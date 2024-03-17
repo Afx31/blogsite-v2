20bd54201de39910af592469efaaff98d6353e1e
Afx31 <post4wjm@gmail.com>
2024-03-17 21:49:03 +1100

    Planetscale removed mainly

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,57 +1,36 @@
 import styles from '../styles/Home.module.css';
-import prisma from '../lib/prisma';
 import Layout from '../components/Layout';
 import { makeSerializable } from '../lib/util'
 import CardDisplay from '../components/CardDisplay/CardDisplay';
 
 const Home = (props) => {
   return (
-    <Layout
-      civicLink={props.navLinks[0].id}
-      ef9Link={props.navLinks[1].id}
-      frogoLink={props.navLinks[2].id}
-      wagoLink={props.navLinks[3].id}
-    >
-      <div className={styles.container}>
-        <div className={styles.innerContainer}>
-          <h1>Latest Posts</h1>
-          <div className={styles.cardContainer}>
-            {props.posts.map((post) => (
-              <CardDisplay
-                key={post.id}
-                post={post}
-              />
-            ))}
-          </div>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <h1>Site under maintenance</h1>
       </div>
-    </Layout>
+    </div>
+    // <Layout
+    //   civicLink={props.navLinks[0].id}
+    //   ef9Link={props.navLinks[1].id}
+    //   frogoLink={props.navLinks[2].id}
+    //   wagoLink={props.navLinks[3].id}
+    // >
+    //   <div className={styles.container}>
+    //     <div className={styles.innerContainer}>
+    //       <h1>Latest Posts</h1>
+    //       <div className={styles.cardContainer}>
+    //         {props.posts.map((post) => (
+    //           <CardDisplay
+    //             key={post.id}
+    //             post={post}
+    //           />
+    //         ))}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </Layout>
   )
-}
-
-export const getServerSideProps = async () => {
-  const posts = await prisma.$queryRaw
-  `
-    SELECT *
-    FROM Post
-    ORDER BY id DESC
-    LIMIT 9
-  `;
-
-  const navLinks = await prisma.$queryRaw
-  `
-    SELECT car, max(id) as id
-    FROM Post
-    GROUP BY car
-    ORDER BY car;
-  `;
-  
-  return {
-    props: {
-      posts: makeSerializable(posts),
-      navLinks:  makeSerializable(navLinks)
-    }
-  }
 }
 
 export default Home;
--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -1,6 +1,5 @@
 import styles from '../../styles/Post.module.css';
 import Router from 'next/router';
-import prisma from '../../lib/prisma';
 import Layout from '../../components/Layout';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router'
@@ -12,82 +11,82 @@ const Post = (props) => {
   const router = useRouter();
   var sortedPosts;
 
-  const GetMonthText = (month) => {
-    var result;
+  // const GetMonthText = (month) => {
+  //   var result;
 
-    switch(month) {
-      case '1':
-        result = 'January';
-        break;
-      case '2':
-        result = 'Febuary';
-        break;
-      case '3':
-        result = 'March';
-        break;
-      case '4':
-        result = 'April';
-        break;
-      case '5':
-        result = 'May';
-        break;
-      case '6':
-        result = 'June';
-        break;
-      case '7':
-        result = 'July';
-        break;
-      case '8':
-        result = 'August';
-        break;
-      case '9':
-        result = 'September';
-        break;
-      case '10':
-        result = 'October';
-        break;
-      case '11':
-        result = 'November';
-        break;
-      case '12':
-        result = 'December';
-        break;
-    }
+  //   switch(month) {
+  //     case '1':
+  //       result = 'January';
+  //       break;
+  //     case '2':
+  //       result = 'Febuary';
+  //       break;
+  //     case '3':
+  //       result = 'March';
+  //       break;
+  //     case '4':
+  //       result = 'April';
+  //       break;
+  //     case '5':
+  //       result = 'May';
+  //       break;
+  //     case '6':
+  //       result = 'June';
+  //       break;
+  //     case '7':
+  //       result = 'July';
+  //       break;
+  //     case '8':
+  //       result = 'August';
+  //       break;
+  //     case '9':
+  //       result = 'September';
+  //       break;
+  //     case '10':
+  //       result = 'October';
+  //       break;
+  //     case '11':
+  //       result = 'November';
+  //       break;
+  //     case '12':
+  //       result = 'December';
+  //       break;
+  //   }
 
-    return result;
-  }
+  //   return result;
+  // }
 
-  const SortPostLinksByMonth = () => {
-    sortedPosts = props.postLinks.reduce((acc, post) => {
-      var date = new Date(post.createdAt);
-      var year = date.getFullYear();
-      var month = date.getMonth() + 1; // Note: Month is 0-indexed, so we add 1 to get the actual month number
-      var key = `${year}-${month}`;
-      var group = acc.find((g) => g.key === key);
+  // const SortPostLinksByMonth = () => {
+  //   sortedPosts = props.postLinks.reduce((acc, post) => {
+  //     var date = new Date(post.createdAt);
+  //     var year = date.getFullYear();
+  //     var month = date.getMonth() + 1; // Note: Month is 0-indexed, so we add 1 to get the actual month number
+  //     var key = `${year}-${month}`;
+  //     var group = acc.find((g) => g.key === key);
 
-      if (group) {
-        group.posts.push(post);
-      } else {
-        acc.push({ key, posts: [post] });
-      }
+  //     if (group) {
+  //       group.posts.push(post);
+  //     } else {
+  //       acc.push({ key, posts: [post] });
+  //     }
 
-      return acc;
-    }, [])
-      .map((g) => ({
-        month: g.key.split("-")[1],
-        year: g.key.split("-")[0],
-        monthYearString: `${GetMonthText(g.key.split("-")[1])} ${g.key.split("-")[0]}`,
-        posts: g.posts
-      }));
-  }
+  //     return acc;
+  //   }, [])
+  //     .map((g) => ({
+  //       month: g.key.split("-")[1],
+  //       year: g.key.split("-")[0],
+  //       monthYearString: `${GetMonthText(g.key.split("-")[1])} ${g.key.split("-")[0]}`,
+  //       posts: g.posts
+  //     }));
+  // }
   
-  SortPostLinksByMonth();
+  // SortPostLinksByMonth();
 
   return router.isFallback ? (
     <Spinner />
   ) : (
     <>
-    <Layout
+    {/* <Layout
       civicLink={props.navLinks[0].id}
       ef9Link={props.navLinks[1].id}
       frogoLink={props.navLinks[2].id}
@@ -137,59 +136,9 @@ const Post = (props) => {
           <PostContentBody post={props.post} />
         </div>
       </div>
-     </Layout>
+     </Layout> */}
     </>
   )
-}
-
-// This function gets called at build time
-export async function getStaticPaths() {
-  const res = await prisma.post.findMany();
-  const posts = await makeSerializable(res);
-  
-  const paths = posts.map((post) => ({
-    params: { id: post.id.toString() }
-  }));
-
-  return { paths, fallback: false }
-}
-
-export async function getStaticProps({ params }) {
-  const post = await prisma.post.findUnique({
-    where: { id: Number(params.id) }
-  })
-  
-  const recentPostLinks = await prisma.post.findMany({
-    where: {
-      car: post.car
-    },
-    orderBy: {
-      id: 'desc'
-    },
-    select: {
-      id: true,
-      title: true,
-      car: true,
-      createdAt: true
-    }
-  })
-
-  const navLinks = await prisma.$queryRaw
-  `
-    SELECT car, max(id) as id
-    FROM Post
-    GROUP BY car
-    ORDER BY car;
-  `;
-
-  return {
-    props: {
-      post: makeSerializable(post),
-      postLinks: makeSerializable(recentPostLinks),
-      navLinks: makeSerializable(navLinks)
-    },
-    revalidate: 600 // In seconds!
-  }
 }
 
 export default Post;
