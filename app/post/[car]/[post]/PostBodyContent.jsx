8d7b723d85dbaf97367d9d37efffc52cefb4af86
Afx31 <post4wjm@gmail.com>
2024-03-26 22:03:44 +1100

    Push from laptop

--- app/post/[car]/[post]/PostBodyContent.jsx
+++ app/post/[car]/[post]/PostBodyContent.jsx
@@ -22,91 +22,98 @@ import ReactMarkdown from 'react-markdown';
 //   );
 // }
 
-export default function PostContentBody({ post }) {
+// export default function PostContentBody({ post }) {
 
-  return (
-    <div>
-      <ReactMarkdown>
-        {post.content}
-      </ReactMarkdown>
-    </div>
-  );
-}
-
-// export default function PostContentBody({ post: {id, title, createdAt, content} }) {
-//   var newContent = content.replaceAll('  ', '  \n')
-//   var slideIndex = 1;
-
-//   useEffect(() => {
-//     if (id !== null) {
-//       carouselRender(slideIndex);
-//     }
-//   }, [id]);
-
-//   function currentSlide(n) {
-//     carouselRender(slideIndex = n)
-//   }
-
-//   function carouselRender(n) {
-//     var slides = document.querySelectorAll(`[src*='carouselimg']`);
-//     var dots = document.querySelectorAll(`[src*='carouseldot']`);
-  
-//     if (slides.length !== 0 && dots.length !== 0) {
-//       var i;
-
-//       dots.forEach((dot) => {
-//         const slideNum = dot.src[dot.src.length-1];
-//         dot.addEventListener('click', (e) => { currentSlide(slideNum) })
-//       });
-  
-//       if (n > slides.length)
-//         slideIndex = 1
-  
-//       if (n < 1)
-//         slideIndex = slides.length
-  
-//       for (i = 0; i < slides.length; i++)
-//         slides[i].style.display = 'none';
-  
-//       for (i = 0; i < dots.length; i++)
-//         dots[i].className = dots[i].className.replace(' active', '');
-  
-//       slides[slideIndex-1].style.display = 'block';
-//       slides[slideIndex-1].style.marginBottom = '-30px';
-//       dots[slideIndex-1].className += ' active';
-//     }
-//   }
-
-//   return id === null || id === undefined ? (
-//     <Spinner />
-//   ) : (
-//     <>
-//       <h1>{title}</h1>
-//       <p className='postDate'>
-//         Posted on{' '}
-//         <Moment format='DD MMMM, YYYY' className='postDateFormat'>
-//           {createdAt}
-//         </Moment>
-//       </p>
-//       <div className='reactMarkdown'>
-//         <ReactMarkdown
-//           className='linebreak'
-//           components={{
-//             a: ({ node, inline, children, href, ...props }) => {
-//               const match = /^YouTube-Link$/.exec(children || "");
-//               return !inline && match ? (
-//                   <YouTubeEmbed src={href} />
-//               ) : (
-//                 <a href={href} {...props}>
-//                   {children}
-//                 </a>
-//               );
-//             },
-//           }}
-//         >
-//           {newContent}
-//         </ReactMarkdown>
-//       </div>
-//     </>
+//   return (
+//     <div>
+//       <ReactMarkdown>
+//         {post.content}
+//       </ReactMarkdown>
+//     </div>
 //   );
 // }
+
+//export default function PostContentBody({ post: {id, title, createdAt, content} }) {
+export default function PostContentBody({ post: {slug, content} }) {
+  // var newContent = content.replaceAll('  ', '  \n')
+  // var slideIndex = 1;
+
+  // useEffect(() => {
+  //   if (id !== null) {
+  //     carouselRender(slideIndex);
+  //   }
+  // }, [id]);
+
+  // function currentSlide(n) {
+  //   carouselRender(slideIndex = n)
+  // }
+
+  // function carouselRender(n) {
+    // var slides = document.querySelectorAll(`[src*='carouselimg']`);
+    // var dots = document.querySelectorAll(`[src*='carouseldot']`);
+  
+    // if (slides.length !== 0 && dots.length !== 0) {
+    //   var i;
+
+    //   dots.forEach((dot) => {
+    //     const slideNum = dot.src[dot.src.length-1];
+    //     dot.addEventListener('click', (e) => { currentSlide(slideNum) })
+    //   });
+  
+    //   if (n > slides.length)
+    //     slideIndex = 1
+  
+    //   if (n < 1)
+    //     slideIndex = slides.length
+  
+    //   for (i = 0; i < slides.length; i++)
+    //     slides[i].style.display = 'none';
+  
+    //   for (i = 0; i < dots.length; i++)
+    //     dots[i].className = dots[i].className.replace(' active', '');
+  
+    //   slides[slideIndex-1].style.display = 'block';
+    //   slides[slideIndex-1].style.marginBottom = '-30px';
+    //   dots[slideIndex-1].className += ' active';
+    // }
+  // }
+
+  return (
+    <>
+      <h1>{slug}</h1>
+      {/* <p className='postDate'>
+        Posted on{' '}
+        <Moment format='DD MMMM, YYYY' className='postDateFormat'>
+          {createdAt}
+        </Moment>
+      </p>
+      <div className='reactMarkdown'>
+        <ReactMarkdown
+          className='linebreak'
+          components={{
+            a: ({ node, inline, children, href, ...props }) => {
+              const match = /^YouTube-Link$/.exec(children || "");
+              return !inline && match ? (
+                  <YouTubeEmbed src={href} />
+              ) : (
+                <a href={href} {...props}>
+                  {children}
+                </a>
+              );
+            },
+          }}
+        >
+          {post.content}
+        </ReactMarkdown>
+      </div> */}
+
+      <div className='reactMarkdown'>        
+        <ReactMarkdown
+
+        >
+          {content}
+        </ReactMarkdown>
+      </div>
+    </>
+  );
+}
--- app/post/[car]/[post]/page.jsx
+++ app/post/[car]/[post]/page.jsx
@@ -5,7 +5,17 @@ import { getPostData, getPostsFiles } from '../../../../lib/util';
 export default function Post({ params }) {
   var sortedPosts;
 
-  console.log('POST: ', params)
+  // const test123 = () => {
+  //   const posts = getPostsFiles(params.car);
+
+  //   return posts.map((post) => ({
+  //     post: post,
+  //   }));
+  // }
+
+  
+  console.log('POST2: ', getPostsFiles(params.car))
+  //console.log('POST: ', params)
 
   const getMonthText = (month) => {
     var result;
@@ -74,8 +84,7 @@ export default function Post({ params }) {
     //     monthYearString: `${GetMonthText(g.key.split("-")[1])} ${g.key.split("-")[0]}`,
     //     posts: g.posts
     //   }));
-  }
-  
+  }  
   //SortPostLinksByMonth();
 
   // return (
@@ -86,57 +95,57 @@ export default function Post({ params }) {
 
   return (
     <div className='vppContainer'>
-        <div className='vppPaneLeft'>
-          <h1>{props.post.car}</h1>
-          <hr className='dropdownDivider'/>
-          <h5>RECENT POSTS</h5>
-          <div className='threadPostLinks'>
-            <ul className='groupedContent'>
-              {sortedPosts.map((groupedPeriod) => (
-                <li className='groupedHeading' key={groupedPeriod.monthYearString}>
-                  <h4>{groupedPeriod.monthYearString}</h4>
-                  <ul className='monthlyPostList'>
-                    {groupedPeriod.posts.map((post) => (
-                      <li key={post.id}>
-                        <NextLink key={post.id} href={{ pathname: `/post/${post.id}` }}>
-                          <a
-                            className={`$'postLinks' ${router.asPath === `/post/${post.id}` ? `${postLinksActive}` : ''} `}
-                          >
-                            {post.title}
-                          </a>
-                        </NextLink>
-                    </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className='vppPaneLeftMobile'>
-          <h1>{props.post.car}</h1>
-          <hr className='dropdownDivider'/>
-          <h5>RECENT POSTS</h5>
-          <div className='threadPostLinks'>
-            <select className='vppSelect' defaultValue={props.post.id} onChange={(e) => Router.push('/post/[id]', `/post/${e.target.value}`)}>
-              {props.postLinks.map((post) => {
-                return <option key={post.id} value={post.id}>{post.title}</option>
-              })}
-            </select>
-          </div>
-        </div>
-        <div className='vppPaneRight'>
-          {/* <PostContentBody post={props.post} /> */}
-          <PostContentBody post={getPostData(params.car, params.post)} />
-        </div>
+      <div className='vppPaneLeft'>
+        <h1>{params.car.toUpperCase()}</h1>
+        <hr className='dropdownDivider'/>
+        <h5>RECENT POSTS</h5>
+        {/* <div className='threadPostLinks'>
+          <ul className='groupedContent'>
+            {sortedPosts.map((groupedPeriod) => (
+              <li className='groupedHeading' key={groupedPeriod.monthYearString}>
+                <h4>{groupedPeriod.monthYearString}</h4>
+                <ul className='monthlyPostList'>
+                  {groupedPeriod.posts.map((post) => (
+                    <li key={post.id}>
+                      <NextLink key={post.id} href={{ pathname: `/post/${post.id}` }}>
+                        <a
+                          className={`$'postLinks' ${router.asPath === `/post/${post.id}` ? `${postLinksActive}` : ''} `}
+                        >
+                          {post.title}
+                        </a>
+                      </NextLink>
+                  </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div> */}
       </div>
+      {/* <div className='vppPaneLeftMobile'>
+        <h1>{props.post.car}</h1>
+        <hr className='dropdownDivider'/>
+        <h5>RECENT POSTS</h5>
+        <div className='threadPostLinks'>
+          <select className='vppSelect' defaultValue={props.post.id} onChange={(e) => Router.push('/post/[id]', `/post/${e.target.value}`)}>
+            {props.postLinks.map((post) => {
+              return <option key={post.id} value={post.id}>{post.title}</option>
+            })}
+          </select>
+        </div>
+      </div> */}
+      <div className='vppPaneRight'>
+        {/* <PostContentBody post={props.post} /> */}
+        <PostContentBody post={getPostData(params.car, params.post)} />
+      </div>
+    </div>
   );
 }
 
-// export async function generateStaticParams() {
-//   const posts = getPostsFiles();
+export async function generateStaticParams() {
+  const posts = getPostsFiles();
 
-//   return posts.map((post) => ({
-//     post: post,
-//   }));
-// }
+  return posts.map((post) => ({
+    post: post,
+  }));
+}
